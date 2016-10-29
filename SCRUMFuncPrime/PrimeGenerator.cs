a8009cd924df82c107fc43f631fad5536ab882df
lzlsqly <lizilikaixin@qq.com>
2016-10-29 14:03:22 +0800

    add a new generateprime class

---
+++ SCRUMFuncPrime/PrimeGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCRUMFuncPrime
+{
+    public static class PrimeGenerator
+    {
+        public static int[] GeneratePrimes(int maxValue)
+        {
+            if (maxValue < 2)
+                return new int[0];
+            var isprime = new bool[maxValue];
+
+            SetmaxValueArray(maxValue, isprime);
+
+            SievePrime(maxValue, isprime);
+
+            return Primes(maxValue, isprime); ;
+        }
+
+        private static void SievePrime(int maxValue, bool[] isprime)
+        {
+            for (var i = 2; i < Math.Sqrt(maxValue) + 1; i++)
+            {
+                for (var j = 2 * i; j < maxValue; j += i)
+                {
+                    isprime[j] = false;
+                }
+            }
+        }
+
+        private static int[] Primes(int maxValue, bool[] isprime)
+        {
+            var primeCount = 0;
+            for (var i = 0; i < maxValue; i++)
+            {
+                if (isprime[i])
+                    primeCount++; 
+            }
+
+            var primes = new int[primeCount];
+            
+            for (int i = 0, j = 0; i < maxValue; i++)
+            {
+                if (isprime[i])
+                    primes[j++] = i;
+            }
+            return primes;
+        }
+
+        private static void SetmaxValueArray(int maxValue, bool[] isprime)
+        {
+            for (int i = 0; i < maxValue; i++)
+            {
+                isprime[i] = true;
+            }
+            isprime[0] = isprime[1] = false;
+        }
+    }
+}
+
---
+++ SCRUMFuncPrimeTests/PrimeGeneratorTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SCRUMFuncPrime;
+
+namespace SCRUMFuncPrimeTests
+{
+    [TestClass]
+    public class PrimeGeneratorTests
+    {
+        public int[] NullArray;
+
+        [TestInitialize]
+        public void Init()
+        {
+            NullArray = new int[24];
+        }
+        [TestMethod()]
+        public void GeneratePrimesTest()
+        {
+            NullArray= PrimeGenerator.GeneratePrimes(0);
+            Assert.AreEqual(0, NullArray.Length);
+
+            NullArray = PrimeGenerator.GeneratePrimes(2);
+            Assert.AreEqual(1, NullArray.Length);
+            Assert.AreEqual(2, NullArray[0]);
+
+            NullArray = PrimeGenerator.GeneratePrimes(3);
+            Assert.AreEqual(2, NullArray.Length);
+            Assert.AreEqual(2, NullArray[0]);
+            Assert.AreEqual(3, NullArray[1]);
+
+            NullArray = PrimeGenerator.GeneratePrimes(100);
+            Assert.AreEqual(25, NullArray.Length);
+            Assert.AreEqual(97, NullArray[24]);
+        }
+    }
+}
